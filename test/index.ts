c3a1f004d1476b747534f37d5cbc05b621a932b4
kin <1037671280@qq.com>
2020-06-10 23:30:05 +0800

    初始化单元测试

--- test/index.ts
+++ test/index.ts
@@ -1,12 +1,14 @@
 import * as chai from "chai";
+import Promise from '../src/promise';
 
 const assert = chai.assert;
 
 describe("Promise", () => {
   it("是一个类", () => {
-
+    assert(typeof Promise === 'function');
+    assert(typeof Promise.prototype === "object");
   });
-  it("拥有对象方法 then", () => {
-
-  })
+  // it("拥有对象方法 then", () => {
+  //
+  // })
 });
